19f0c071262dad539c7453b5c3dcf8ecfdcfca88
Admin <valerasarkisan285@gmail.com>
2026-05-19 14:14:47 +0300

    py-restore-names

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -4,33 +4,31 @@ from app.restore_names import restore_names
 
 @pytest.mark.parametrize(
     "users, expected",
-    [
-        (
-            [
-                {
-                    "first_name": None,
-                    "last_name": "Holy",
-                    "full_name": "Jack Holy",
-                },
-                {
-                    "last_name": "Adams",
-                    "full_name": "Mike Adams",
-                },
-            ],
-            [
-                {
-                    "first_name": "Jack",
-                    "last_name": "Holy",
-                    "full_name": "Jack Holy",
-                },
-                {
-                    "first_name": "Mike",
-                    "last_name": "Adams",
-                    "full_name": "Mike Adams",
-                },
-            ],
-        )
-    ],
+    [(
+        [
+            {
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+            {
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            },
+        ],
+        [
+            {
+                "first_name": "Jack",
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+            {
+                "first_name": "Mike",
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            },
+        ],
+    )],
 )
 def test_restore_names(users: list, expected: list) -> None:
     restore_names(users)
